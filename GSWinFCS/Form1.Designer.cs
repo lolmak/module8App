5ac7625eef24767c54e58b59a47885b5fd9e8eb9
ValOrTina <lolmakGS@ya.ru>
2025-02-14 19:15:21 +0700

    Fix курсора

--- GSWinFCS/Form1.Designer.cs
+++ GSWinFCS/Form1.Designer.cs
@@ -43,19 +43,21 @@ namespace GSWinFCS
             this.generalText.AutoSize = true;
             this.generalText.BackColor = System.Drawing.Color.Honeydew;
             this.generalText.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.generalText.Location = new System.Drawing.Point(57, 9);
+            this.generalText.Location = new System.Drawing.Point(86, 14);
+            this.generalText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.generalText.Name = "generalText";
-            this.generalText.Size = new System.Drawing.Size(548, 39);
+            this.generalText.Size = new System.Drawing.Size(816, 61);
             this.generalText.TabIndex = 0;
             this.generalText.Text = "Для начала войдите в систему";
             // 
             // loginName
             // 
             this.loginName.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.loginName.Location = new System.Drawing.Point(150, 99);
+            this.loginName.Location = new System.Drawing.Point(225, 152);
+            this.loginName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.loginName.MaxLength = 35;
             this.loginName.Name = "loginName";
-            this.loginName.Size = new System.Drawing.Size(368, 36);
+            this.loginName.Size = new System.Drawing.Size(550, 50);
             this.loginName.TabIndex = 1;
             this.loginName.TextChanged += new System.EventHandler(this.loginName_TextChanged);
             this.loginName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.loginName_KeyPress);
@@ -65,9 +67,10 @@ namespace GSWinFCS
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Honeydew;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(147, 80);
+            this.label2.Location = new System.Drawing.Point(220, 123);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(51, 16);
+            this.label2.Size = new System.Drawing.Size(70, 25);
             this.label2.TabIndex = 2;
             this.label2.Text = "логин";
             // 
@@ -76,9 +79,10 @@ namespace GSWinFCS
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Honeydew;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(147, 165);
+            this.label1.Location = new System.Drawing.Point(220, 254);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 16);
+            this.label1.Size = new System.Drawing.Size(83, 25);
             this.label1.TabIndex = 4;
             this.label1.Text = "пароль";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -86,19 +90,22 @@ namespace GSWinFCS
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(150, 184);
+            this.textBox1.Location = new System.Drawing.Point(225, 283);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.MaxLength = 20;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(368, 36);
+            this.textBox1.Size = new System.Drawing.Size(550, 50);
             this.textBox1.TabIndex = 3;
             this.textBox1.UseSystemPasswordChar = true;
             // 
             // button1
             // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(150, 259);
+            this.button1.Location = new System.Drawing.Point(225, 398);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(141, 36);
+            this.button1.Size = new System.Drawing.Size(212, 55);
             this.button1.TabIndex = 5;
             this.button1.Text = "Войти";
             this.button1.UseVisualStyleBackColor = true;
@@ -107,21 +114,23 @@ namespace GSWinFCS
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Honeydew;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label3.ForeColor = System.Drawing.Color.Blue;
-            this.label3.Location = new System.Drawing.Point(325, 279);
+            this.label3.Location = new System.Drawing.Point(488, 429);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(193, 16);
+            this.label3.Size = new System.Drawing.Size(264, 25);
             this.label3.TabIndex = 6;
             this.label3.Text = "или зарегистрироваться";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Honeydew;
-            this.ClientSize = new System.Drawing.Size(660, 331);
+            this.ClientSize = new System.Drawing.Size(990, 509);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -129,6 +138,7 @@ namespace GSWinFCS
             this.Controls.Add(this.label2);
             this.Controls.Add(this.loginName);
             this.Controls.Add(this.generalText);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "GS Module8 App";
             this.ResumeLayout(false);
